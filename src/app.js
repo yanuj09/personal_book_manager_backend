95884e978c8f2784fe14571c9f0c5fa95354b06a
Anuj Yadav <anuj.yadav@vishleshan.net>
2026-08-05 15:39:31 +0530

    chore: add package.json for personal book manager backend with dependencies and scripts

--- src/app.js
+++ src/app.js
@@ -2,6 +2,29 @@ const express = require('express');
 const app = express();
 const port = 4000;
 
+
+// Middleware to parse JSON body
+app.use(express.json());
+
+app.get("/login" , (req,res) => {
+
+  const { username, password } = req.body;
+
+  if (username === "nirahu" && password === "1234") {
+    res.send("Login successful");
+  }
+})
+
+app.post("/signup" , (req,res) => {
+
+  const {firstName, lastName, DOB ,email, password} = req.body;
+
+  if (firstName && lastName && email && password) {
+    res.send(`Signup successful for ${firstName} ${lastName} with email ${email}`);
+  }
+
+})
+
 app.get('/', (req, res) => {
   res.send('Hello Nirahu!');
 });
