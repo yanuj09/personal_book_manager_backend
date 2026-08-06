9b5e48e73eb925f69d5950b197ebf2212937e8e5
Anuj Yadav <anuj.yadav@vishleshan.net>
2026-08-06 19:20:45 +0530

    fix: remove trailing slash from allowed origin URL in CORS configuration

--- src/app.js
+++ src/app.js
@@ -13,7 +13,7 @@ const app = express();
 const port = process.env.PORT || 3567;
 const allowedOrigins = [
   process.env.FRONTEND_URL,
-  'https://personal-book-manager-ui-neon.vercel.app/',
+  'https://personal-book-manager-ui-neon.vercel.app',
   'http://localhost:3000',
   'http://127.0.0.1:3000',
 
